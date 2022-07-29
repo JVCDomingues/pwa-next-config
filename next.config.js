7fafaa4bcdf76da9e6568c39509a97ebd33bb1e5
João Victor C. Domingues <joao.domingues@b2wdigital.com>
2022-07-29 12:08:09 -0300

    Adding components from Chakra and sound effect

--- next.config.js
+++ next.config.js
@@ -9,6 +9,16 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
     dest: 'public'
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: 'file-loader',
+      },
+    })
+
+    return config
   }
 })
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../../styles/globals.css'
 import Head  from 'next/head'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
       <Head>
         <meta name='application-name' content='JVCDomingues PWA' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
