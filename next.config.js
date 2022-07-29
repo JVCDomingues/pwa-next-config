08ae3c1f4c63023b3f222bbc51d9216b2e709115
João Victor C. Domingues <joao.domingues@b2wdigital.com>
2022-07-29 11:24:41 -0300

    Setting up PWA config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  }
+})
+
 module.exports = nextConfig
